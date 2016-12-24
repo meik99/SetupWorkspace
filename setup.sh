45671257a538a2e4d6674ed26e2b6aa9cb73990c
Michael <michaelrynkiewicz3@gmail.com>
2016-12-24 15:41:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,31 @@ if [ "$EUID" -ne 0 ] then
   exit
 fi
 
+#set username
+username="mrynkiewicz"
+
+#update and upgrade system
 apt update && apt upgrade -yf
+
+#create temporary work folder
+mkdir tmp
+cd tmp
+
+#download java 
+#thanks to 
+#http://stackoverflow.com/questions/10268583/downloading-java-jdk-on-linux-via-wget-is-shown-license-page-instead
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz
+tar -xf jdk-8u112-linux-x64.tar.gz
+
+#change owner of /opt to my username
+chown -R $(username):$(username) /opt
+chown -R $(username):$(username) /opt/*
+mv jdk1.8.0_112 /opt/jdk
+
+#export JAVA_HOME
+echo "export JAVA_HOME=/opt/jdk/" >> /home/$(username)/.bashrc
+source /home/$(username)/.bashrc
+
+#add alias
+echo "alias java='/opt/jdk/bin/java'" >> /home/$(username)/.bashrc
+source /home/$(username)/.bashrc
